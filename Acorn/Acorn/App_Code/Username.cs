51d5c8db4b428873b02c71eaf9f90830d79e7b1e
P1NGV <zero_black_2@hotmail.com>
2016-10-22 14:38:14 -0600

    css added
    design cosas

---
+++ Acorn/Acorn/App_Code/Username.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Username
+/// </summary>
+public class Username
+{
+	public Username()
+	{
+		//
+		// TODO: Agregar aquí la lógica del constructor
+		//
+	}
+    public long idUser;
+
+
+    public string user;
+
+
+    public string password;
+}
